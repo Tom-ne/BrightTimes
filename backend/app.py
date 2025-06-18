687ae05a223ebdee5b1d330e06ba7deb65aeec13
Tom Neumann <tomn505@gmail.com>
2025-06-18 17:48:53 +0300

    feat: add authentication and activity management routes with JWT protection

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from db import engine, Base
 from routes.activity_routes import activity_routes_blueprint
+from routes.auth_routes import auth_routes_blueprint
+from routes.organizer_routes import organizer_routes_blueprint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["ALGORITHM"] = os.getenv("ALGORITHM")
+
+# Register Blueprints
 app.register_blueprint(activity_routes_blueprint)
+app.register_blueprint(auth_routes_blueprint)
+app.register_blueprint(organizer_routes_blueprint)
 
 # Create DB tables
 Base.metadata.create_all(bind=engine)
